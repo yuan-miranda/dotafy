7c4f01148bbbec5bde60267f1673cb04450a713e
yuan-miranda <yuanezekielamiranda@gmail.com>
2024-05-15 22:45:40 +0800

    removed the test files.

--- test/steamIdtoDotaId.js
+++
@@ -1,7 +0,0 @@
-function getDota2IdBySteamId(steamId) {
-    const steam64Base = BigInt("76561197960265728");
-    return (BigInt(steamId) - steam64Base).toString();
-}
-
-console.log(getDota2IdBySteamId("76561199401129478"));
-console.log(getDota2IdBySteamId("76561198103151062"));
